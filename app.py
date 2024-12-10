bfb55220857fb53172bdc1b4853ddd38655c306e
Nico <ngomez@hey.com>
2024-12-10 18:58:15 -0300

    :sparkles: ejemplo de variable en template

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('landing.html')
+
+
+@app.route('/user/<username>')
+def user(username):
+    favorite_movies = [
+        'The Shawshank Redemption',
+        'The Godfather',
+        'The Dark Knight',
+    ]
+    return render_template('user.html', username=username, favorite_movies=favorite_movies)
